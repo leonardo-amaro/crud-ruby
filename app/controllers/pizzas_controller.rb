37706d077bb7a1e34e3f6c554c46d90d9e12a787
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-08 12:15:40 +0000

    corrigindo mensagens no controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -25,7 +25,7 @@ class PizzasController < ApplicationController
 
     respond_to do |format|
       if @pizza.save
-        format.html { redirect_to @pizza, notice: "Pizza criada com sucesso!" }
+        format.html { redirect_to @pizza, notice: "Pizza was successfully created" }
         format.json { render :show, status: :created, location: @pizza }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class PizzasController < ApplicationController
   def update
     respond_to do |format|
       if @pizza.update(pizza_params)
-        format.html { redirect_to @pizza, notice: "Pizza atualizada com sucesso!", status: :see_other }
+        format.html { redirect_to @pizza, notice: "Pizza was successfully updated", status: :see_other }
         format.json { render :show, status: :ok, location: @pizza }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class PizzasController < ApplicationController
     @pizza.destroy!
 
     respond_to do |format|
-      format.html { redirect_to pizzas_path, notice: "Pizza apagada com sucesso!", status: :see_other }
+      format.html { redirect_to pizzas_path, notice: "Pizza was successfully destroyed", status: :see_other }
       format.json { head :no_content }
     end
   end
