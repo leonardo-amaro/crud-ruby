d4085e74524f484108897c975d8c6ff0084ae693
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-09 12:35:54 +0000

    configurando API e CORS

---
+++ backend/config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # ou coloque o domínio do Vue
+    resource '*', headers: :any, methods: %i[get post put patch delete options head]
+  end
+end
