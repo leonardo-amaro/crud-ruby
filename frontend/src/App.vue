14543d1e2b23cb8743f2fce05c58cab1c66671b9
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-10 17:30:43 +0000

    add(front): estililzando os cards do cardápio

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,24 @@ import Cardapio from './components/Cardapio.vue';
 
 <template>
   <h1>Pizzaria CRUD</h1>
-  <Cardapio />
+  <main>
+    <Cardapio />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  padding: 1.75rem 4rem;
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+main {
+  background-color: #fdfdfd;
+  width: 80%;
+  margin: auto;
+  padding: 1.75rem;
+  border-radius: 16px;
+  box-shadow: 0px -2px 8px rgba(0, 0, 0, 0.3);
+}
+</style>
--- frontend/src/components/Cardapio.vue
+++ frontend/src/components/Cardapio.vue
@@ -15,8 +15,52 @@ onMounted(async () => {
     <h1>Cardápio</h1>
     <ul>
       <li v-for="pizza in pizzas" :key="pizza.id">
-        {{ pizza.nome }} — R$ {{ pizza.preco }}
+        <figure>
+          <img src="/images/pizza_cartoon.jpg" alt="Imagem de {{ pizza.nome }}">
+          <figcaption>
+            <h3>{{ pizza.nome }}</h3>
+            <h4>R$ {{ pizza.preco }}</h4>
+          </figcaption>
+        </figure>
       </li>
     </ul>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  margin-bottom: 2rem;
+}
+
+ul {
+  padding: 8px;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  gap: 16px;
+}
+
+figure {
+  border: 1px solid #000;
+  border-radius: 16px;
+  transition: margin-top 300ms;
+  overflow: clip;
+}
+
+figure:hover {
+  cursor: pointer;
+  margin-top: -8px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+figcaption {
+  padding: 0.75rem;
+}
+
+h4 {
+  text-align: right;
+}
+</style>
