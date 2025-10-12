8051958bf863a051481bd85b48c2cc4c5c2fe6a5
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-12 17:20:51 +0000

    add(front): estilizando form

--- frontend/src/components/PizzaForm.vue
+++ frontend/src/components/PizzaForm.vue
@@ -24,19 +24,65 @@ async function createPizza() {
 </script>
 
 <template>
+  <h1>Adicionar Pizza</h1>
+
   <form @submit.prevent="createPizza">
-    <div>
+    <div class="campo-form">
       <label>Nome:</label>
       <input v-model="pizza.nome" type="text" required />
     </div>
 
-    <div>
+    <div class="campo-form">
       <label>Preço:</label>
       <input v-model.number="pizza.preco" type="number" required />
     </div>
 
-    <button type="submit">Adicionar Pizza</button>
+    <div class="campo-form">
+      <button type="submit">Adicionar Pizza</button>
+      <router-link to="/" id="cancelar">Cancelar</router-link>
+    </div>
   </form>
 
   <p v-if="mensagem">{{ mensagem }}</p>
 </template>
+
+<style scoped>
+h1 {
+  margin-bottom: 2rem;
+}
+
+.campo-form {
+  padding: 0.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+input {
+  width: 100%;
+  margin: 0 0.75rem;
+  padding: 1.25rem 0.5rem;
+}
+
+button {
+  background-color: #9acd32;
+  padding: 0.75rem;
+  border: none;
+  border-radius: 8px;
+  font-size: medium;
+  transition: background-color 300ms;
+}
+
+#cancelar {
+  color: #ff6347;
+  text-decoration: none;
+  padding: 0.75rem;
+  border: 1px solid #ff6347;
+  border-radius: 8px;
+}
+
+button:hover {
+  background-color: #afe73e;
+  cursor: pointer;
+}
+</style>
