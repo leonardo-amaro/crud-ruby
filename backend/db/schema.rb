3074e12bc253e9b80ae507b8335c10c8399ca94c
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-23 12:25:53 +0000

    add(back): criando Model para User

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_03_121441) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_23_122142) do
   create_table "pizzas", force: :cascade do |t|
     t.string "nome"
     t.decimal "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
