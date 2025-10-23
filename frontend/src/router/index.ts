3a49884afca0a9608692057784b0e033d7ec78f2
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-23 12:41:32 +0000

    add(front): protegendo rotas no Vue

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,11 +7,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Home', component: HomeView },
+    { path: '/', name: 'Home', component: HomeView, meta: { requiresAuth: true } },
     { path: '/new', name: 'NewPizza', component: NewPizzaView },
     { path: '/pizza/:id', name: 'InfoPizza', component: InfoPizza, props: true },
     { path: '/pizza/:id/edit', name: 'EditPizza', component: EditPizzaView, props: true }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
